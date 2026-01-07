fc26247c4ae44b3fc345a417134e3595368c383a
andykimpe1 <andykimpe@gmail.com>
2026-01-07 17:49:20 +0100

    Create 3560.php

---
+++ p/3560.php
@@ -0,0 +1,65 @@
+<article id="post-3560" class="post-3560 post type-post status-publish format-standard has-post-thumbnail hentry category-pokemon-sun-moon category-release">
+	<header class="entry-header entry-header-single">
+		<h1 class="entry-title">Pokémon Sun &#038; Moon 067 VOSTFR + Pokémon The Movie</h1>		<div class="entry-meta entry-meta-1"><span class="author vcard"><a class="url fn n" href="https://pokemonfansub.alwaysdata.net/?author=5">grimm-THERA</a></span><span class="entry-date"><time class="entry-date published" datetime="2018-04-06T17:21:11+00:00">6 avril 2018</time></span><span class="comments-link"><i class="fa fa-comments-o"></i><a href="https://pokemonfansub.alwaysdata.net/?p=3560#comments">26</a></span></div>	</header><!-- .entry-header -->
+
+
+	<div class="entry-content">
+		<p>Long métrage Alola !</p>
+<p>Alors oui, voici l&rsquo;épisode 67. De ce fait, on rattrape le retard, mais ce n&rsquo;est pas le plus important.<br />
+Enfin, la surprise qu&rsquo;on a teasée plus longtemps qu&rsquo;un jeu Square Enix est là !<br />
+Certains d&rsquo;entre-vous l&rsquo;auront peut-être déjà devinée, mais il s&rsquo;agit du film &laquo;&nbsp;Pokémon : Je te choisis !&nbsp;&raquo;.</p>
+<p>Avec comme d&rsquo;habitude une version avec les noms FR et une autre avec les noms JAP. Là où il y a une petite différence, et c&rsquo;est ce qui nous a pris toute la semaine, on propose aussi du 720p et du 1080p ! Nos ordinateurs ont saigné pour vous offrir ces petits bijoux (8h et 20h pour les encodes sans parler du temps pris aux autres étapes) !</p>
+<p>Profitez-en bien, on vous retrouvera pour l&rsquo;épisode 68 !</p>
+<figure style="width: 800px;" class="wp-caption alignnone"><img class="" src="https://i.ytimg.com/vi/SAiU_qQLrNk/maxresdefault.jpg" alt="" width="800" height="450" /><figcaption class="wp-caption-text">Lève les bras si toi aussi tu vas être abandonné dans une forêt à la fin de la saison</figcaption></figure>
+<p>SM 067 : 
+
+
+
+<!--<a href="https://mega.nz/#!BJYWRB5b!WwxlyIj-MSd444_hEj5FfgrRQWU7LaUX1eppIso9qPg">Pokémon en français</a>-->
+
+<a href="https://pokemonfansub.alwaysdata.net/download/_Pokemon_Fansub__Pokemon_SunMoon_-_67_VOSTFR__FR___TV_720p_AAC.mp4.php">Pokémon en français</a>
+
+
+<br />
+
+
+</br></br></br></br></br></br><iframe width="640" height="180" frameborder="0" src="https://pokemonfansub.alwaysdata.net/video.php?link=https://pokemonfansub.alwaysdata.net/download/_Pokemon_Fansub__Pokemon_SunMoon_-_67_VOSTFR__FR___TV_720p_AAC.mp4.php" allowfullscreen></iframe>
+
+
+
+
+
+</p>
+<figure style="width: 405px;" class="wp-caption alignnone"><img class="" src="https://www.themoviedb.org/t/p/w600_and_h900_face/igwVelU3AXon7yDL82vHNOJS4ic.jpg" alt="" width="405" height="599" /><figcaption class="wp-caption-text">Vous, s&rsquo;extasiant devant le film</figcaption></figure>
+<p><span style="text-decoration: underline;">Pocket Monster the Movie 2017 720p</span> :</p>
+<p><a href="https://mega.nz/#!RUBnWaAY!27uMFr4cJ6KUxsVR8W7pjzV79FmxXRBDydbnDEnQxIY">Pokémon en français</a>, <a href="https://nyaa.si/view/1023740">Torrent</a><br />
+<a href="https://mega.nz/#!ddBmRArb!wdfT4g_K2JUIEXVQf4h8EjXULbnEMJkVnPqESrXM4fA">Pokémon en japonais</a> et <a href="https://nyaa.si/view/1023735">Torrent</a></p>
+<p><span style="text-decoration: underline;">Pocket Monster the Movie 2017 1080p</span> :</p>
+<p><a href="https://mega.nz/#!5ZgHSCgJ!_2ExneSpV5zQlEYXYFqjVcNLzcIo173naPNDv3sVfv0">Pokémon en français</a> et <a href="https://nyaa.si/view/1023854">Torrent</a> &#8211; <a href="https://uptobox.com/4xubxquics0s" target="_blank">UptoBox</a><br />
+<a href="https://mega.nz/#!MBI3GSAb!kSEUWaYdittkuDWfSxG-aZsROK05Fkh9OZDrJ-pVEyo">Pokémon en japonais</a> et <a href="https://nyaa.si/view/1023853">Torrent</a> &#8211; <a href="https://uptobox.eu/y5y6gsu81bay" target="_blank">UptoBox</a></p>
+			</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<div class="entry-taxonomies"><div class="entry-categories"><span>Categories</span><ul class="post-categories">
+	<li><a href="https://pokemonfansub.alwaysdata.net/?cat=15" rel="category">Pokémon Sun &amp; Moon</a></li>
+	<li><a href="https://pokemonfansub.alwaysdata.net/?cat=6" rel="category">Release</a></li></ul></div></div>
+						<div class="post-navigation row">
+			<div class="col-md-6">
+								<span>Previous article</span>
+				<h2 class="h5"><a href="https://pokemonfansub.alwaysdata.net/?p=3550" rel="prev">Pokémon Sun &#038; Moon 066 VOSTFR</a></h2>
+							</div>
+			<div class="col-md-6 post-navi-next">
+								<span>Next article</span>
+				<h2 class="h5"><a href="https://pokemonfansub.alwaysdata.net/?p=3596" rel="next">Pokémon Sun &#038; Moon 068, 069 et 070 VOSTFR</a></h2>
+							</div>
+		</div>
+		
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
+
+<!-- <div id="comments" class="comments-area"> -->					
+
+
+				
+				</main><!-- #main -->
+			</div><!-- #primary -->
