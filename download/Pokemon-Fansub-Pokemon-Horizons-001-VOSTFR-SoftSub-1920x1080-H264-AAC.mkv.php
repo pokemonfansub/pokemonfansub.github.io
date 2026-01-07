b420a63f280c9a503d0d0250a4f75d9d0fee0c69
andykimpe1 <andykimpe@gmail.com>
2026-01-07 18:57:10 +0100

    Update Pokemon-Fansub-Pokemon-Horizons-001-VOSTFR-SoftSub-1920x1080-H264-AAC.mkv.php

--- download/Pokemon-Fansub-Pokemon-Horizons-001-VOSTFR-SoftSub-1920x1080-H264-AAC.mkv.php
+++ download/Pokemon-Fansub-Pokemon-Horizons-001-VOSTFR-SoftSub-1920x1080-H264-AAC.mkv.php
@@ -1,6 +1,6 @@
 <?php
 include "../function.php";
-$name = "[Pokémon Fansub] Pokémon Horizons (Pocket Monsters 2023) - Partie 1 - Le départ de Liko et Rhod (001-025) [VOSTFR SoftSub]/[Pokémon Fansub] Pokémon Horizons (Pocket Monsters 2023) - Partie 1 - Le départ de Liko et Rhod (001-025) [VOSTFR SoftSub]/[Pokémon Fansub] Pokémon Horizons - 001 (VOSTFR-SoftSub 1920x1080 H264 AAC).mkv";
+$name = "[Pokémon Fansub] Pokémon Horizons (Pocket Monsters 2023) - Partie 1 - Le départ de Liko et Rhod (001-025) [VOSTFR SoftSub]/[Pokémon Fansub] Pokémon Horizons (Pocket Monsters 2023) - Partie 1 - Le départ de Liko et Rhod (001-025) [VOSTFR SoftSub]/[Pokémon Fansub] Pokémon Horizons - 001 (VOSTFR-SoftSub 1920x1080 H264 AAC).mkv";                                                                                                            
 $url = "https://yourprivatehosting/torrent/debrid.php?name=" . urlencode($name) . "";
 $cr = curl($url, null, "false", "false", null, null);
 header("Location: " . $cr . "");
