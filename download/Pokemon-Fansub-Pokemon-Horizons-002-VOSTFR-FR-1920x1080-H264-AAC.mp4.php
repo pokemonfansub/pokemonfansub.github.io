7b7c033ada227df0b9d16b96c9b7a71ff984e4b4
andykimpe1 <andykimpe@gmail.com>
2026-01-07 18:51:53 +0100

    Update Pokemon-Fansub-Pokemon-Horizons-002-VOSTFR-FR-1920x1080-H264-AAC.mp4.php

--- download/Pokemon-Fansub-Pokemon-Horizons-002-VOSTFR-FR-1920x1080-H264-AAC.mp4.php
+++ download/Pokemon-Fansub-Pokemon-Horizons-002-VOSTFR-FR-1920x1080-H264-AAC.mp4.php
@@ -1,10 +1,7 @@
 <?php
 include "../function.php";
 $name = "[Pokémon Fansub] Pokémon Horizons - 002 (VOSTFR-FR 1920x1080 H264 AAC).mp4";
-$url = https://yourprivatehosting/torrent/debrid.php?name=" . urlencode($name) . "";
-$post = http_build_query([
-    'link' => "" . $link . "",
-]);
-$cr = curl($url, null, "false", "false", null, $post);
+$url = "https://yourprivatehosting/torrent/debrid.php?name=" . urlencode($name) . "";
+$cr = curl($url, null, "false", "false", null, null);
 header("Location: " . $cr . "");
 exit;
