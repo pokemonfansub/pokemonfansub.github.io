fa271f6e4140c1662d8c028a01b747254f1e30d3
andykimpe1 <andykimpe@gmail.com>
2026-01-07 19:41:17 +0100

    Add files via upload

---
+++ download/Pokemon-Fansub-Pokemon-Horizons-007-VOSTFR-SoftSub-1920x1080-H264-AAC.mkv.php
@@ -0,0 +1,7 @@
+<?php
+include "../function.php";
+ = "[Pokémon Fansub] Pokémon Horizons (Pocket Monsters 2023) - Partie 1 - Le départ de Liko et Rhod (001-025) [VOSTFR SoftSub]/[Pokémon Fansub] Pokémon Horizons (Pocket Monsters 2023) - Partie 1 - Le départ de Liko et Rhod (001-025) [VOSTFR SoftSub]/[Pokémon Fansub] Pokémon Horizons - 007 (VOSTFR-SoftSub 1920x1080 H264 AAC).mkv";
+$url = "https://yourprivatehosting/torrent/debrid.php?name=" . urlencode($name) . "";
+$cr = curl($url, null, "false", "false", null, null);
+header("Location: " . $cr . "");
+exit;
