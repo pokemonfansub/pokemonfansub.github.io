73013de84ac0cc2615b4ef7e7d331ff9f9c5502e
andykimpe1 <andykimpe@gmail.com>
2026-01-07 18:35:56 +0100

    Create Pokemon-Fansub-Pokemon-Horizons-004-VOSTFR-FR-1920x1080-H264-AAC.mp4.php

---
+++ download/Pokemon-Fansub-Pokemon-Horizons-004-VOSTFR-FR-1920x1080-H264-AAC.mp4.php
@@ -0,0 +1,10 @@
+<?php
+include "../function.php";
+$name = "[Pokémon Fansub] Pokémon Horizons - 004 (VOSTFR-JAP 1920x1080 H264 AAC).mp4";
+$url = https://yourprivatehosting/torrent/debrid.php?name=" . urlencode($name) . "";
+$post = http_build_query([
+    'link' => "" . $link . "",
+]);
+$cr = curl($url, null, "false", "false", null, $post);
+header("Location: " . $cr . "");
+exit;
