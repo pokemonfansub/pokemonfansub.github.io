2722a64a5896872af85805333ccdfc9c3f890650
andykimpe1 <andykimpe@gmail.com>
2026-01-08 14:42:15 +0100

    Create 8267.php

---
+++ p/8267.php
@@ -0,0 +1,47 @@
+<!--<article star>-->
+<article id="post-8267" class="post-8267 post type-post status-publish format-standard has-post-thumbnail hentry category-pokemon-horizons category-release">
+	<header class="entry-header entry-header-single">
+		<h1 class="entry-title">Pokémon Horizons 110 et 111 VOSTFR</h1>		<div class="entry-meta entry-meta-1"><span class="author vcard"><a class="url fn n" href="https://pokemonfansub.alwaysdata.net/?author=2">Trizek</a></span><span class="entry-date"><time class="entry-date published updated" datetime="2025-10-03T05:54:36+00:00">3 octobre 2025</time></span><span class="comments-link"><i class="fa fa-comments-o"></i><a href="https://pokemonfansub.alwaysdata.net/?p=8267#comments">8</a></span></div>	</header><!-- .entry-header -->
+
+
+	<div class="entry-content">
+		<p>Bonjour à tous !</p>
+<p>Les Explorateurs ont tendu un piège aux Électacleurs Volants. Les voilà maintenant obligés d&rsquo;affronter les Six Héros, boostés par ce qu&rsquo;il semble être des Sphères de Puissance améliorées&#8230; Comment vont-ils s&rsquo;en sortir ?</p>
+<p>Bon visionnage !</p>
+<p>PS : La diffusion de l&rsquo;animé au Japon est actuellement en pause, elle reprendra le 10 octobre avec 3 épisodes flash-backs centrés sur Rhod et Uruto à Kalos !</p>
+<p><img class="" src="https://pokemonfansub.alwaysdata.net/wp-content/uploads/HZ110.png" alt="" width="686" height="385" /></p>
+<p>PMHZ 110, Pokémon en français : <a href="https://mega.nz/file/2AclGZza#zmSdT6cBUtn4JC-3W_cH3srWCXVJAExWhLiHm6Ml7ls">Mega</a> et <a href="https://nyaa.si/view/2026352">Torrent</a><br />
+PMHZ 110, Pokémon en japonais : <a href="https://mega.nz/file/Wc9WTQqI#gcVNd003VApHBgptYBu5umLs8iCg1d1XvXWBdsLv_KA">Mega</a> et <a href="https://nyaa.si/view/2026353">Torrent</a></p>
+<p><img class="" src="https://pokemonfansub.alwaysdata.net/wp-content/uploads/HZ111.png" alt="" width="686" height="385" /></p>
+<p>PMHZ 111, Pokémon en français : <a href="https://mega.nz/file/mIMmCIQL#WlCJAG1hKdtDcRVDKstNTK5bohvQ5K7EqMFGUQHEAGo">Mega</a> et <a href="https://nyaa.si/view/2026354">Torrent</a><br />
+PMHZ 111, Pokémon en japonais : <a href="https://mega.nz/file/nYExQDYS#g94sgDzPxMLCT2cBIcSnUouY44ihm93DXuYJcCxc8Bk">Mega</a> et <a href="https://nyaa.si/view/2026355">Torrent</a></p>
+<p>Streaming : le streaming est disponible sur Mega après quelques minutes de traitement.</p>
+			</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<div class="entry-taxonomies"><div class="entry-categories"><span>Categories</span><ul class="post-categories">
+	<li><a href="https://pokemonfansub.alwaysdata.net/?cat=21" rel="category">Pokémon Horizons</a></li>
+	<li><a href="https://pokemonfansub.alwaysdata.net/?cat=6" rel="category">Release</a></li></ul></div></div>
+						<div class="post-navigation row">
+			<div class="col-md-6">
+								<span>Previous article</span>
+				<h2 class="h5"><a href="https://pokemonfansub.alwaysdata.net/?p=8254" rel="prev">Pokémon Horizons 108 et 109 VOSTFR</a></h2>
+							</div>
+			<div class="col-md-6 post-navi-next">
+								<span>Next article</span>
+				<h2 class="h5"><a href="https://pokemonfansub.alwaysdata.net/?p=8282" rel="next">Pokémon Horizons 112, 113 et 114 VOSTFR</a></h2>
+							</div>
+		</div>
+		
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
+
+					
+<!-- <div id="comments" class="comments-area"> -->					
+
+
+				
+				</main><!-- #main -->
+			</div><!-- #primary -->
+			
+<!--<article end>-->
