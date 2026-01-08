f637b2fc22c52839ce5cb0231e8be8d28f571f8f
andykimpe1 <andykimpe@gmail.com>
2026-01-08 14:56:30 +0100

    Update 8282.php

--- p/8282.php
+++ p/8282.php
@@ -1,3 +1,4 @@
+<!--<article star>-->
 <article id="post-8282" class="post-8282 post type-post status-publish format-standard has-post-thumbnail hentry category-pokemon-horizons category-release">
 	<header class="entry-header entry-header-single">
 		<h1 class="entry-title">Pokémon Horizons 112, 113 et 114 VOSTFR</h1>		<div class="entry-meta entry-meta-1"><span class="author vcard"><a class="url fn n" href="https://pokemonfansub.alwaysdata.net/?author=2">Trizek</a></span><span class="entry-date"><time class="entry-date published updated" datetime="2025-11-08T04:20:24+00:00">8 novembre 2025</time></span><span class="comments-link"><i class="fa fa-comments-o"></i><a href="https://pokemonfansub.alwaysdata.net/?p=8282#comments">15</a></span></div>	</header><!-- .entry-header -->
@@ -131,3 +132,4 @@ et <a href="https://nyaa.si/view/2040064">Torrent</a></p>
 				
 				</main><!-- #main -->
 			</div><!-- #primary -->
+<!--<article end>-->
