63be9401eb8f3af2e0229c4f232804df8d4dc566
andykimpe1 <andykimpe@gmail.com>
2026-01-08 21:33:42 +0100

    Update generate-html.sh

--- generate-html.sh
+++ generate-html.sh
@@ -37,19 +37,21 @@ echo $Nombre
 
 
 cat $HOME/pokemonfansub.github.io/include/head.html > $HOME/pokemonfansub.github.io/p/$Nombreedit.html
-if [ -f $HOME/pokemonfansub.github.io/p/$Nombreedit.php  ]; then
-   cat $HOME/pokemonfansub.github.io/p/$Nombreedit.php >> $HOME/pokemonfansub.github.io/p/$Nombreedit.html
+if [ -f $HOME/pokemonfansub.github.io/ph/$Nombreedit.php  ]; then
+   mkdir -p $HOME/pokemonfansub.github.io/phtml/
+   cat $HOME/pokemonfansub.github.io/p/$Nombreedit.php >> $HOME/pokemonfansub.github.io/phtml/$Nombreedit.html
    sed -i "s|https://pokemonfansub.alwaysdata.net/video.php?link=https://pokemonfansub.alwaysdata.net/download/Pokemon-Fansub-Pokemon-Horizons-$Nombreedit-VOSTFR-FR-1920x1080-H264-AAC.mp4.php&poster=https://pokemonfansub.alwaysdata.net/wp-content/uploads/HZ$Nombreedit.png|https://pokemonfansub.github.io/video/Pokemon-Fansub-Pokemon-Horizons-$Nombreedit-VOSTFR-FR-1920x1080-H264-AAC.mp4.html|g" $HOME/pokemonfansub.github.io/p/$Nombreedit.html
    sed -i "s|https://pokemonfansub.alwaysdata.net/video.php?link=https://pokemonfansub.alwaysdata.net/download/Pokemon-Fansub-Pokemon-Horizons-$Nombreedit-VOSTFR-JAP-1920x1080-H264-AAC.mp4.php&poster=https://pokemonfansub.alwaysdata.net/wp-content/uploads/HZ$Nombreedit.png|https://pokemonfansub.github.io/video/Pokemon-Fansub-Pokemon-Horizons-$Nombreedit-VOSTFR-FR-1920x1080-H264-AAC.mp4.html|g" $HOME/pokemonfansub.github.io/p/$Nombreedit.html
-   sed -i 's|#respond">|.html|g' $HOME/pokemonfansub.github.io/p/$Nombreedit.html
+   sed -i 's|#respond">|.html|g' $HOME/pokemonfansub.github.io/phtml/$Nombreedit.html
    sed -i 's|" rel="prev">|.html" rel="prev">|g' $HOME/pokemonfansub.github.io/p/$Nombreedit.html
-   sed -i 's|?p=|p/|g' $HOME/pokemonfansub.github.io/p/$Nombreedit.html
-   sed -i 's|https://pokemonfansub.alwaysdata.net/|https://pokemonfansub.github.io/|g' $HOME/pokemonfansub.github.io/p/$Nombreedit.html
+   sed -i 's|?p=|phtml/|g' $HOME/pokemonfansub.github.io/phtml/$Nombreedit.html
+   sed -i 's|https://pokemonfansub.alwaysdata.net/|https://pokemonfansub.github.io/|g' $HOME/pokemonfansub.github.io/phtml/$Nombreedit.html
 else
-   cat $HOME/pokemonfansub.github.io/include/home.html >> $HOME/pokemonfansub.github.io/p/$Nombreedit.html
+   mkdir -p $HOME/pokemonfansub.github.io/phtml/
+   cat $HOME/pokemonfansub.github.io/include/home.html >> $HOME/pokemonfansub.github.io/phtml/$Nombreedit.html
 fi
-cat $HOME/pokemonfansub.github.io/include/secondary.html >> $HOME/pokemonfansub.github.io/p/$Nombreedit.html
-cat $HOME/pokemonfansub.github.io/include/footer.html >> $HOME/pokemonfansub.github.io/p/$Nombreedit.html
+cat $HOME/pokemonfansub.github.io/include/secondary.html >> $HOME/pokemonfansub.github.io/phtml/$Nombreedit.html
+cat $HOME/pokemonfansub.github.io/include/footer.html >> $HOME/pokemonfansub.github.io/phtml/$Nombreedit.html
     ((Nombre++))
 done
 
