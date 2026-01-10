d63f7d6de3c20a2c397915fb727a2520df9fd1d2
andykimpe1 <andykimpe@gmail.com>
2026-01-10 21:41:48 +0100

    Update home.php

--- include/home.php
+++ include/home.php
@@ -7,7 +7,7 @@
         <a class="entry-category" href="https://pokemonfansub.alwaysdata.net/?cat=21">Pokémon Horizons</a>    </div>
     <div class="entry-detail">
         <header class="entry-header">
-    		<h2 class="entry-title"><a href="https://pokemonfansub.alwaysdata.net/?p=8346" rel="bookmark">Pokémon Horizons 121 et 122 VOSTFR</a></h2>    		<div class="entry-meta entry-meta-1"><span class="author vcard"><a class="url fn n" href="https://pokemonfansub.alwaysdata.net/?author=2">Trizek</a></span><span class="entry-date"><time class="entry-date published" datetime="2026-01-06T01:28:54+00:00">6 janvier 2026</time></span><span class="comments-link"><i class="fa fa-comments-o"></i><a href="https://pokemonfansub.alwaysdata.net/?p=8346#respond">0</a></span></div>    	</header><!-- .entry-header -->
+    		<h2 class="entry-title"><a href="https://pokemonfansub.alwaysdata.net/?p=8346" rel="bookmark">Pokémon Horizons 121 et 122 VOSTFR</a></h2>    		<div class="entry-meta entry-meta-1"><span class="entry-date"><time class="entry-date published" datetime="2026-01-06T01:28:54+00:00">6 janvier 2026</time></span></div>    	</header><!-- .entry-header -->
 
     	<div class="entry-excerpt">
     		<div class="ft-excerpt">Bonjour à tous ! Friede a refait surface ! Il a contacté les autres Électacleurs Volants pour les prévenir de...</div>    	</div><!-- .entry-content -->
@@ -21,7 +21,7 @@
         <a class="entry-category" href="https://pokemonfansub.alwaysdata.net/?cat=21">Pokémon Horizons</a>    </div>
     <div class="entry-detail">
         <header class="entry-header">
-    		<h2 class="entry-title"><a href="https://pokemonfansub.alwaysdata.net/?p=8335" rel="bookmark">Pokémon Horizons 119 et 120 VOSTFR</a></h2>    		<div class="entry-meta entry-meta-1"><span class="author vcard"><a class="url fn n" href="https://pokemonfansub.alwaysdata.net/?author=2">Trizek</a></span><span class="entry-date"><time class="entry-date published updated" datetime="2025-12-23T08:07:56+00:00">23 décembre 2025</time></span><span class="comments-link"><i class="fa fa-comments-o"></i><a href="https://pokemonfansub.alwaysdata.net/?p=8335#comments">4</a></span></div>    	</header><!-- .entry-header -->
+    		<h2 class="entry-title"><a href="https://pokemonfansub.alwaysdata.net/?p=8335" rel="bookmark">Pokémon Horizons 119 et 120 VOSTFR</a></h2>    		<div class="entry-meta entry-meta-1"><span class="entry-date"><time class="entry-date published updated" datetime="2025-12-23T08:07:56+00:00">23 décembre 2025</time></span></div>    	</header><!-- .entry-header -->
 
     	<div class="entry-excerpt">
     		<div class="ft-excerpt">Bonjour à tous ! L'entraînement à l'Institut Myrtille se poursuit, et touche bientôt à sa fin ! Il semblerait...</div>    	</div><!-- .entry-content -->
@@ -35,7 +35,7 @@
         <a class="entry-category" href="https://pokemonfansub.alwaysdata.net/?cat=21">Pokémon Horizons</a>    </div>
     <div class="entry-detail">
         <header class="entry-header">
-    		<h2 class="entry-title"><a href="https://pokemonfansub.alwaysdata.net/?p=8321" rel="bookmark">Pokémon Horizons 117 et 118 VOSTFR</a></h2>    		<div class="entry-meta entry-meta-1"><span class="author vcard"><a class="url fn n" href="https://pokemonfansub.alwaysdata.net/?author=2">Trizek</a></span><span class="entry-date"><time class="entry-date published updated" datetime="2025-12-08T07:51:32+00:00">8 décembre 2025</time></span><span class="comments-link"><i class="fa fa-comments-o"></i><a href="https://pokemonfansub.alwaysdata.net/?p=8321#comments">6</a></span></div>    	</header><!-- .entry-header -->
+    		<h2 class="entry-title"><a href="https://pokemonfansub.alwaysdata.net/?p=8321" rel="bookmark">Pokémon Horizons 117 et 118 VOSTFR</a></h2>    		<div class="entry-meta entry-meta-1"><span class="entry-date"><time class="entry-date published updated" datetime="2025-12-08T07:51:32+00:00">8 décembre 2025</time></span></div>    	</header><!-- .entry-header -->
 
     	<div class="entry-excerpt">
     		<div class="ft-excerpt">Bonjour à tous ! Liko, Rhod, Dot et Uruto ont commencé leur entraînement à l'Institut Myrtille sous la direction...</div>    	</div><!-- .entry-content -->
@@ -49,7 +49,7 @@
         <a class="entry-category" href="https://pokemonfansub.alwaysdata.net/?cat=21">Pokémon Horizons</a>    </div>
     <div class="entry-detail">
         <header class="entry-header">
-    		<h2 class="entry-title"><a href="https://pokemonfansub.alwaysdata.net/?p=8304" rel="bookmark">Pokémon Horizons 115 et 116 VOSTFR</a></h2>    		<div class="entry-meta entry-meta-1"><span class="author vcard"><a class="url fn n" href="https://pokemonfansub.alwaysdata.net/?author=2">Trizek</a></span><span class="entry-date"><time class="entry-date published updated" datetime="2025-12-04T04:33:34+00:00">4 décembre 2025</time></span><span class="comments-link"><i class="fa fa-comments-o"></i><a href="https://pokemonfansub.alwaysdata.net/?p=8304#comments">7</a></span></div>    	</header><!-- .entry-header -->
+    		<h2 class="entry-title"><a href="https://pokemonfansub.alwaysdata.net/?p=8304" rel="bookmark">Pokémon Horizons 115 et 116 VOSTFR</a></h2>    		<div class="entry-meta entry-meta-1"><span class="entry-date"><time class="entry-date published updated" datetime="2025-12-04T04:33:34+00:00">4 décembre 2025</time></span></div>    	</header><!-- .entry-header -->
 
     	<div class="entry-excerpt">
     		<div class="ft-excerpt">Bonjour à tous ! Les Électacleurs Volants prennent un nouvel envol, direction l'Institut Myrtille à Unys ! Liko,...</div>    	</div><!-- .entry-content -->
@@ -63,7 +63,7 @@
         <a class="entry-category" href="https://pokemonfansub.alwaysdata.net/?cat=21">Pokémon Horizons</a>    </div>
     <div class="entry-detail">
         <header class="entry-header">
-    		<h2 class="entry-title"><a href="https://pokemonfansub.alwaysdata.net/?p=8282" rel="bookmark">Pokémon Horizons 112, 113 et 114 VOSTFR</a></h2>    		<div class="entry-meta entry-meta-1"><span class="author vcard"><a class="url fn n" href="https://pokemonfansub.alwaysdata.net/?author=2">Trizek</a></span><span class="entry-date"><time class="entry-date published updated" datetime="2025-11-08T04:20:24+00:00">8 novembre 2025</time></span><span class="comments-link"><i class="fa fa-comments-o"></i><a href="https://pokemonfansub.alwaysdata.net/?p=8282#comments">15</a></span></div>    	</header><!-- .entry-header -->
+    		<h2 class="entry-title"><a href="https://pokemonfansub.alwaysdata.net/?p=8282" rel="bookmark">Pokémon Horizons 112, 113 et 114 VOSTFR</a></h2>    		<div class="entry-meta entry-meta-1"><span class="entry-date"><time class="entry-date published updated" datetime="2025-11-08T04:20:24+00:00">8 novembre 2025</time></span></div>    	</header><!-- .entry-header -->
 
     	<div class="entry-excerpt">
     		<div class="ft-excerpt">Bonjour à tous ! Après une petite pause de la série, mais aussi de certains d'entre nous pour cause de vacances,...</div>    	</div><!-- .entry-content -->
@@ -77,7 +77,7 @@
         <a class="entry-category" href="https://pokemonfansub.alwaysdata.net/?cat=21">Pokémon Horizons</a>    </div>
     <div class="entry-detail">
         <header class="entry-header">
-    		<h2 class="entry-title"><a href="https://pokemonfansub.alwaysdata.net/?p=8267" rel="bookmark">Pokémon Horizons 110 et 111 VOSTFR</a></h2>    		<div class="entry-meta entry-meta-1"><span class="author vcard"><a class="url fn n" href="https://pokemonfansub.alwaysdata.net/?author=2">Trizek</a></span><span class="entry-date"><time class="entry-date published updated" datetime="2025-10-03T05:54:36+00:00">3 octobre 2025</time></span><span class="comments-link"><i class="fa fa-comments-o"></i><a href="https://pokemonfansub.alwaysdata.net/?p=8267#comments">8</a></span></div>    	</header><!-- .entry-header -->
+    		<h2 class="entry-title"><a href="https://pokemonfansub.alwaysdata.net/?p=8267" rel="bookmark">Pokémon Horizons 110 et 111 VOSTFR</a></h2>    		<div class="entry-meta entry-meta-1"><span class="entry-date"><time class="entry-date published updated" datetime="2025-10-03T05:54:36+00:00">3 octobre 2025</time></span></div>    	</header><!-- .entry-header -->
 
     	<div class="entry-excerpt">
     		<div class="ft-excerpt">Bonjour à tous ! Les Explorateurs ont tendu un piège aux Électacleurs Volants. Les voilà maintenant obligés...</div>    	</div><!-- .entry-content -->
@@ -91,7 +91,7 @@
         <a class="entry-category" href="https://pokemonfansub.alwaysdata.net/?cat=21">Pokémon Horizons</a>    </div>
     <div class="entry-detail">
         <header class="entry-header">
-    		<h2 class="entry-title"><a href="https://pokemonfansub.alwaysdata.net/?p=8254" rel="bookmark">Pokémon Horizons 108 et 109 VOSTFR</a></h2>    		<div class="entry-meta entry-meta-1"><span class="author vcard"><a class="url fn n" href="https://pokemonfansub.alwaysdata.net/?author=2">Trizek</a></span><span class="entry-date"><time class="entry-date published updated" datetime="2025-09-19T18:42:20+00:00">19 septembre 2025</time></span><span class="comments-link"><i class="fa fa-comments-o"></i><a href="https://pokemonfansub.alwaysdata.net/?p=8254#comments">7</a></span></div>    	</header><!-- .entry-header -->
+    		<h2 class="entry-title"><a href="https://pokemonfansub.alwaysdata.net/?p=8254" rel="bookmark">Pokémon Horizons 108 et 109 VOSTFR</a></h2>    		<div class="entry-meta entry-meta-1"><span class="entry-date"><time class="entry-date published updated" datetime="2025-09-19T18:42:20+00:00">19 septembre 2025</time></span></div>    	</header><!-- .entry-header -->
 
     	<div class="entry-excerpt">
     		<div class="ft-excerpt">Bonjour à tous ! La grand-mère de Liko, Diana, fait son grand retour ! De toute évidence, nos héros vont avoir...</div>    	</div><!-- .entry-content -->
@@ -105,7 +105,7 @@
         <a class="entry-category" href="https://pokemonfansub.alwaysdata.net/?cat=21">Pokémon Horizons</a>    </div>
     <div class="entry-detail">
         <header class="entry-header">
-    		<h2 class="entry-title"><a href="https://pokemonfansub.alwaysdata.net/?p=8245" rel="bookmark">Pokémon Horizons 107 VOSTFR</a></h2>    		<div class="entry-meta entry-meta-1"><span class="author vcard"><a class="url fn n" href="https://pokemonfansub.alwaysdata.net/?author=2">Trizek</a></span><span class="entry-date"><time class="entry-date published" datetime="2025-09-06T15:53:22+00:00">6 septembre 2025</time></span><span class="comments-link"><i class="fa fa-comments-o"></i><a href="https://pokemonfansub.alwaysdata.net/?p=8245#comments">8</a></span></div>    	</header><!-- .entry-header -->
+    		<h2 class="entry-title"><a href="https://pokemonfansub.alwaysdata.net/?p=8245" rel="bookmark">Pokémon Horizons 107 VOSTFR</a></h2>    		<div class="entry-meta entry-meta-1"><span class="entry-date"><time class="entry-date published" datetime="2025-09-06T15:53:22+00:00">6 septembre 2025</time></span></div>    	</header><!-- .entry-header -->
 
     	<div class="entry-excerpt">
     		<div class="ft-excerpt">Bonjour à tous ! Uruto a de nouveau croisé la route des Explorateurs ! Il leur court après, tandis que Rhod part...</div>    	</div><!-- .entry-content -->
@@ -119,7 +119,7 @@
         <a class="entry-category" href="https://pokemonfansub.alwaysdata.net/?cat=21">Pokémon Horizons</a>    </div>
     <div class="entry-detail">
         <header class="entry-header">
-    		<h2 class="entry-title"><a href="https://pokemonfansub.alwaysdata.net/?p=8235" rel="bookmark">Pokémon Horizons 106 VOSTFR</a></h2>    		<div class="entry-meta entry-meta-1"><span class="author vcard"><a class="url fn n" href="https://pokemonfansub.alwaysdata.net/?author=2">Trizek</a></span><span class="entry-date"><time class="entry-date published updated" datetime="2025-08-29T23:31:11+00:00">29 août 2025</time></span><span class="comments-link"><i class="fa fa-comments-o"></i><a href="https://pokemonfansub.alwaysdata.net/?p=8235#comments">7</a></span></div>    	</header><!-- .entry-header -->
+    		<h2 class="entry-title"><a href="https://pokemonfansub.alwaysdata.net/?p=8235" rel="bookmark">Pokémon Horizons 106 VOSTFR</a></h2>    		<div class="entry-meta entry-meta-1"><span class="entry-date"><time class="entry-date published updated" datetime="2025-08-29T23:31:11+00:00">29 août 2025</time></span></div>    	</header><!-- .entry-header -->
 
     	<div class="entry-excerpt">
     		<div class="ft-excerpt">Bonjour à tous ! Il semblerait qu'il y ait quelques tensions à bord du Brave Asagi... Uruto part de son côté et...</div>    	</div><!-- .entry-content -->
@@ -133,7 +133,7 @@
         <a class="entry-category" href="https://pokemonfansub.alwaysdata.net/?cat=21">Pokémon Horizons</a>    </div>
     <div class="entry-detail">
         <header class="entry-header">
-    		<h2 class="entry-title"><a href="https://pokemonfansub.alwaysdata.net/?p=8224" rel="bookmark">Pokémon Horizons 105 VOSTFR</a></h2>    		<div class="entry-meta entry-meta-1"><span class="author vcard"><a class="url fn n" href="https://pokemonfansub.alwaysdata.net/?author=2">Trizek</a></span><span class="entry-date"><time class="entry-date published updated" datetime="2025-08-26T07:13:30+00:00">26 août 2025</time></span><span class="comments-link"><i class="fa fa-comments-o"></i><a href="https://pokemonfansub.alwaysdata.net/?p=8224#comments">5</a></span></div>    	</header><!-- .entry-header -->
+    		<h2 class="entry-title"><a href="https://pokemonfansub.alwaysdata.net/?p=8224" rel="bookmark">Pokémon Horizons 105 VOSTFR</a></h2>    		<div class="entry-meta entry-meta-1"><span class="entry-date"><time class="entry-date published updated" datetime="2025-08-26T07:13:30+00:00">26 août 2025</time></span></div>    	</header><!-- .entry-header -->
 
     	<div class="entry-excerpt">
     		<div class="ft-excerpt">Bonjour à tous ! Les Électacleurs Volants se retrouvent à présent en bord de mer ! Il semblerait qu'un Gromago...</div>    	</div><!-- .entry-content -->
