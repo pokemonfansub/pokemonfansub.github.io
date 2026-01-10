28c0490700ad42559415c4528c127c5105ab5fbb
andykimpe1 <andykimpe@gmail.com>
2026-01-10 23:51:08 +0100

    Update generate-html.sh

--- generate-html.sh
+++ generate-html.sh
@@ -2,35 +2,35 @@
 cd $HOME
 rm -rf $HOME/pokemonfansub.github.io
 git clone git@github.com:pokemonfansub/pokemonfansub.github.io.git
-Nombre=1
-while [ $Nombre -le 122 ]
-do
-if [ ${#Nombre} == 1 ]
-then
-    Nombreedit=00$Nombre
-elif [ ${#Nombre} == 2 ]
-then
-    Nombreedit=0$Nombre
-else
-    Nombreedit=$Nombre
-fi
-echo $Nombre
-cat > $HOME/pokemonfansub.github.io/video/Pokemon-Fansub-Pokemon-Horizons-$Nombreedit-VOSTFR-FR-1920x1080-H264-AAC.mp4.html <<EOF
-<video poster="https://pokemonfansub.github.io/wp-content/uploads/HZ$Nombreedit.png" controls width="100%" height="100%"/>
-    <source src="https://pokemonfansub.alwaysdata.net/download/Pokemon-Fansub-Pokemon-Horizons-$Nombreedit-VOSTFR-FR-1920x1080-H264-AAC.mp4.php" type="video/mp4">
-</video>
-EOF
-cat > $HOME/pokemonfansub.github.io/video/Pokemon-Fansub-Pokemon-Horizons-$Nombreedit-VOSTFR-JAP-1920x1080-H264-AAC.mp4.html <<EOF
-<video poster="https://pokemonfansub.github.io/wp-content/uploads/HZ$Nombreedit.png" controls width="100%" height="100%"/>
-    <source src="https://pokemonfansub.alwaysdata.net/download/Pokemon-Fansub-Pokemon-Horizons-$Nombreedit-VOSTFR-JAP-1920x1080-H264-AAC.mp4.php" type="video/mp4">
-</video>
-EOF
-    ((Nombre++))
-done
+#Nombre=1
+#while [ $Nombre -le 122 ]
+#do
+#if [ ${#Nombre} == 1 ]
+#then
+#    Nombreedit=00$Nombre
+#elif [ ${#Nombre} == 2 ]
+#then
+#    Nombreedit=0$Nombre
+#else
+#    Nombreedit=$Nombre
+#fi
+#echo $Nombre
+#cat > $HOME/pokemonfansub.github.io/video/Pokemon-Fansub-Pokemon-Horizons-$Nombreedit-VOSTFR-FR-1920x1080-H264-AAC.mp4.html <<EOF
+#<video poster="https://pokemonfansub.github.io/wp-content/uploads/HZ$Nombreedit.png" controls width="100%" height="100%"/>
+#    <source src="https://pokemonfansub.alwaysdata.net/download/Pokemon-Fansub-Pokemon-Horizons-$Nombreedit-VOSTFR-FR-1920x1080-H264-AAC.mp4.php" type="video/mp4">
+#</video>
+#EOF
+#cat > $HOME/pokemonfansub.github.io/video/Pokemon-Fansub-Pokemon-Horizons-$Nombreedit-VOSTFR-JAP-1920x1080-H264-AAC.mp4.html <<EOF
+#<video poster="https://pokemonfansub.github.io/wp-content/uploads/HZ$Nombreedit.png" controls width="100%" height="100%"/>
+#    <source src="https://pokemonfansub.alwaysdata.net/download/Pokemon-Fansub-Pokemon-Horizons-$Nombreedit-VOSTFR-JAP-1920x1080-H264-AAC.mp4.php" type="video/mp4">
+#</video>
+#EOF
+#    ((Nombre++))
+#done
 
 
-Nombre=1
-while [ $Nombre -le 9999 ]
+Nombre=8345
+while [ $Nombre -le 8347 ]
 do
 Nombreedit=$Nombre
 echo $Nombre
@@ -44,12 +44,14 @@ cat "$HOME/pokemonfansub.github.io/include/head.html" > "$HOME/pokemonfansub.git
 EOF
 
 if [ -f "$HOME/pokemonfansub.github.io/phhtml/$Nombreedit.js"  ]; then
+   echo "file "$HOME/pokemonfansub.github.io/phhtml/$Nombreedit.js" exist"
    cat >> "$HOME/pokemonfansub.github.io/phtml/$Nombreedit.html"<<EOF
 <!--<article star>-->
 <script src="https://pokemonfansub.github.io/phhtml/$Nombreedit.js"></script>
 <!--<article end>-->
 EOF
 else
+   echo "file "$HOME/pokemonfansub.github.io/phhtml/$Nombreedit.js" not exist"
    cat >> "$HOME/pokemonfansub.github.io/phtml/$Nombreedit.html"<<EOF
 <!--<article star>-->
 <script src="https://pokemonfansub.github.io/include/home.js"></script>
