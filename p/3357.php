8ea99bb2c07e7d0769b0e88f79f2a51e30a01fdb
andykimpe1 <andykimpe@gmail.com>
2026-01-12 23:35:29 +0100

    Create 3357.php

---
+++ p/3357.php
@@ -0,0 +1,31 @@
+<article id="post-3357" class="post-3357 page type-page status-publish hentry">
+	<header class="entry-header">
+		<h1 class="entry-title">Arc XY</h1>	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<p>Salut tout le monde,</p>
+<p>Pour ceux qui ne sont pas au courant, 90% des liens dans les anciennes news sur XY et XY&amp;Z sont morts.<br />
+Nous avons donc décidé d&rsquo;uploader ces épisodes-là en Torrent<strong>*</strong>, voici la liste des liens :</p>
+<p><del><strong>XY</strong> : Torrent <a href="https://nyaa.si/view/964019" target="_blank">01 à 23</a> &#8211; <a href="https://nyaa.si/view/964020" target="_blank">24 à 46</a> &#8211; <a href="https://nyaa.si/view/964021" target="_blank"> 47 à 69</a> &#8211; <a href="https://nyaa.si/view/964022" target="_blank">70 à 93</a> &#8211; UptoBox <a href="https://uptobox.com/user_public?hash=88301e831e3fdb85&amp;folder=3119644859" target="_blank">Saison 17</a> &#8211; <a href="https://uptobox.com/user_public?hash=b02bc6ab88e81e6a&amp;folder=3063611579" target="_blank">Saison 18</a></del><br />
+<del> <small><em>Épisode VOSTFR (noms JAP) : Ep 1 à 11 &#8211; 28 à 31 &#8211; 33 &#8211; 38 à 51 &#8211; 53 à 80 &#8211; 82 à 93</em><br />
+<em>Épisode VOSTFR (noms FR) : Ep 12 à 27 &#8211; 32 &#8211; 34 à 37 &#8211; 52 &#8211; 81</em></small></del><br />
+<del> <strong>XY&amp;Z</strong> : Torrent <a href="https://nyaa.si/view/964042" target="_blank">01 à 27</a> &#8211; <a href="https://nyaa.si/view/964025" target="_blank">28 à 49</a> &#8211; UptoBox <a href="https://uptobox.com/user_public?hash=c3ea45a7bb636e4b&amp;folder=3057713339" target="_blank">Saison 19</a></del><br />
+<del> <small><em>Complètement en VOSTFR-JAP</em></small></del></p>
+<p><del><strong>*</strong> Maj 05/05/2023 : Ajout des liens UptoBox.</del></p>
+<p>* Maj du 11/06/2023 : De nombreux épisodes ont été retrouvés !<br />
+<strong>Saison 17 :</strong> UptoBox <a href="https://uptobox.eu/user_public?hash=284731de1dcb63da&amp;folder=1446510790" target="_blank">STFR-FR</a> <em><small>012 à 043 + 046 à 049</small></em> / <a href="https://uptobox.eu/user_public?hash=284731de1dcb63da&amp;folder=1446510790" target="_blank">STFR-JAP</a> <em><small>001 à 011 + 044 à 045</small></em><br />
+<strong>Saison 18 :</strong> UptoBox <a href="https://uptobox.eu/user_public?hash=df8d9afd1089b943&amp;folder=966721735" target="_blank">STFR-FR</a> <em><small>Complet</small></em> / <a href="https://uptobox.eu/user_public?hash=c4198939229a25a1&amp;folder=2033057990" target="_blank">STFR-JAP</a> <em><small>50 + 53 à 54 + 56 à 80 + 82 à 93</small></em><br />
+<strong>Saison 19 :</strong> UptoBox <a href="https://uptobox.eu/user_public?hash=eb9042fb0c3cefcd&amp;folder=2687697095" target="_blank">STFR-FR</a> <em><small>Complet</small></em> / <a href="https://uptobox.eu/user_public?hash=ed8681682b1b12d1&amp;folder=282460360" target="_blank">STFR-JAP</a> <em><small>Complet</small></em></p>
+<p>Pour le moment, ce sera la seule option de téléchargement que nous proposerons, nous avons des priorités plus importantes que de ré-uploader tous les épisodes sur d&rsquo;autres plateformes.</p>
+<p>Les releases proviennent de la <strong>Saikou Fansub</strong>, de la <strong>Arrow Fansub</strong> et de la <strong>Pokémon Fansub</strong>.</p>
+			</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+			</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
+
+<!-- <div id="comments" class="comments-area"> -->
+					
+				</main><!-- #main -->
+			</div><!-- #primary -->
+<!--<article end>-->
