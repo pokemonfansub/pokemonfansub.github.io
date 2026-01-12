c0951b11cf68ce191091de6d82b24c7dc91afcbb
andykimpe1 <andykimpe@gmail.com>
2026-01-12 23:24:54 +0100

    Create 3848.php

---
+++ p/3848.php
@@ -0,0 +1,75 @@
+<article id="post-3848" class="post-3848 page type-page status-publish hentry">
+	<header class="entry-header">
+		<h1 class="entry-title">Arc Advanced Génération</h1>	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<p><strong>Saison 6</strong><br />
+<a href="/?page_id=5291"><em>Streaming</em></a><br />
+<em>Téléchargement :</em><br />
+Packs Torrent : <a href="https://nyaa.si/view/1631043">Noms FR</a> / <a href="https://nyaa.si/view/1631051">Noms JAP<br />
+</a>Dossier UptoBox : <a href="https://uptobox.com/user_public?hash=977996391a2e01b2&amp;folder=3616080059">Noms FR</a> / <a href="https://uptobox.com/user_public?hash=c4a791623a6665aa&amp;folder=3656057019">Noms JAP</a></p>
+<p><a href="/?p=3851">AG 001</a> : Une nouvelle terre ! Une nouvelle aventure !!<br />
+<a href="/?p=3895">AG 002</a> : Le Pokémon antique et la mystérieuse Team !<br />
+<a href="/?p=3913">AG 003</a> : L&rsquo;arène de Clémenti-Ville ! VS Vigoroth !!<br />
+<a href="/?p=3920">AG 004</a> : Plein de Nirondelle, plein de danger ! Capture dans le Bois-Clémenti !!<br />
+<a href="/?p=3939">AG 005</a> : Zigzaton et le Gamin en Short ! Le premier combat de Flora !!<br />
+<a href="/?p=3951">AG 006</a> : La Team Rocket ! Des adieux aux Combo-Griffes !!<br />
+<a href="/?p=3961">AG 007</a> : La forêt des Arcko ! Protéger l&rsquo;arbre géant !!<br />
+<a href="/?p=3989">AG 008</a> : Séviper VS Arcko ! L&rsquo;attaque Écras&rsquo;Face de la mort !!<br />
+<a href="/?p=4010">AG 009</a> : Étrange ! Le mystère du manoir des Balignon !!<br />
+<a href="/?p=4024">AG 010</a> : Le Bekipan le plus puissant du monde !!<br />
+<a href="/?p=4032">AG 011</a> : Grahyèna et Medhyèna ! Le mystère de l&rsquo;évolution !!<br />
+<a href="/?p=4051">AG 012</a> : Nénupiot et les trois sœurs du Fleuriste Jolie Fleur !<br />
+<a href="/?p=4062">AG 013</a> : Concours Pokémon ! Le magnifique combat de Charmillon !!<br />
+<a href="/?p=4082">AG 014</a> : Double Combat et double Chenipotte !?<br />
+<a href="/?p=4093">AG 015</a> : C&rsquo;est l&rsquo;heure des cours ! L&rsquo;école des dresseurs Pokémon !!<br />
+<a href="/?p=4111">AG 016</a> : L&rsquo;arène de Mérouville ! L&rsquo;arme secrète de Tarinor !!<br />
+<a href="/?p=4125">AG 017</a> : La Société Devon ! L&rsquo;ombre de la Team Aqua !!<br />
+<a href="/?p=4144">AG 018</a> : M. Marco et Piko, le Goélise !<br />
+<a href="/?p=4163">AG 019</a> : Fuite ! L&rsquo;île des Sharpedo !!<br />
+<a href="/?p=4177">AG 020</a> : L&rsquo;arène de Myokara ! Bastien, le Champion surfeur !<br />
+<a href="/?p=4177">AG 021</a> : Chenipotte vs Chenipotte ! Qui est qui !?<br />
+<a href="/?p=4210">AG 022</a> : Pierre, Galekid, Galeking !<br />
+<a href="/?p=4236">AG 023</a> : Le Pokémon déchaîné du bord de mer, Écrapince !<br />
+<a href="/?p=4256">AG 024</a> : Cours, Sacha ! Traverser la rivière aux Carvanha !!<br />
+<a href="/?p=4274">AG 025</a> : Le lac mystérieux ! Des Gobou à foison !?<br />
+<a href="/?p=4274">AG 026</a> : L&rsquo;attaque de la tribu des Pifeuil !<br />
+<a href="/?p=4314">AG 027</a> : Team Magma vs Team Aqua ! Le combat de la Base Secrète !<br />
+<a href="/?p=4329">AG 028</a> : Charmillon et Papinox ! Aux limites de l&rsquo;évolution !!<br />
+<a href="/?p=4346">AG 029</a> : Frayeur avec Ténéfix !<br />
+<a href="/?p=4361">AG 030</a> : La Combattante et Méditikka ! Pris dans la tempête !<br />
+<a href="/?p=4376">AG 031</a> : Revanche à l&rsquo;arène de Myokara ! Le terrain de surf !<br />
+<a href="/?p=4386">AG 032</a> : L&rsquo;Épave ! L&rsquo;ombre qui rôde !!<br />
+<a href="/?p=4406">AG 033</a> : Un rival pour Flora ! Entraînement aux Concours Pokémon !!<br />
+<a href="/?p=4417">AG 034</a> : Les Pokémon de départ sèment la panique !!<br />
+<a href="/?p=4445">AG 035</a> : Flora ! Le premier Concours Pokémon !!<br />
+<a href="/?p=4469">AG 036</a> : Protéger le Musée Océanographique ! L&rsquo;attaque de la Team Magma !!<br />
+<a href="/?p=4478">AG 037</a> : La belle et la bête !? Tengalice et Joëlle !<br />
+<a href="/?p=4502">AG 038</a> : Posipi et Négapi ! Le phare de la montagne !!<br />
+<a href="/?p=4539">AG 039</a> : En chanson ! La Maison des Pièges Pokémon !!<br />
+<a href="/?p=4541">AG 040</a> : L&rsquo;arène de Lavandia ! Match électrique contre Voltère !!</p>
+<p><strong>Saison 7 : Advanced Challenge<br />
+</strong><a href="/?page_id=5298"><em>Streaming</em></a><br />
+<em>Téléchargement :</em><br />
+Dossier UptoBox : <a href="https://uptobox.com/user_public?hash=a3730cbac74abe7d&amp;folder=3628204219">Noms FR</a> / <a href="https://uptobox.com/user_public?hash=55b52ac6650bbc63&amp;folder=1555235004">Noms JAP</a></p>
+<p><a href="/?p=4619">AG 041</a> : La nouvelle attaque d&rsquo;Arcko !! La Balle Graine du champ de pastèques !<br />
+<a href="/?p=4638">AG 042</a> : Muciole et Lumivole ! La danse de l&rsquo;amour !<br />
+<a href="/?p=4657">AG 043</a> : Vole, Draby ! Vers le futur !!<br />
+<a href="/?p=4671">AG 044</a> : Voilà Ondine ! Togepi et le Royaume Mirage !!<br />
+<a href="/?p=4701">AG 045</a> : De l&rsquo;autre côté du mirage ! Le Paradis Togepi !!<br />
+<a href="/?p=4716">AG 046</a> : La Famille Stratège ! 4 vs 4 !!<br />
+<a href="/?p=4735">AG 047</a> : Skitty et l&rsquo;aromathérapie !<br />
+<a href="/?p=4817">AG 048</a> : Mangriff vs Séviper ! Confrontation entre rivaux !!<br />
+<a href="/?p=4836">AG 049</a> : Max et Max ! Protéger Arakdo !!<br />
+<a href="/?p=4855">AG 050</a> : Concours Pokémon ! Le tournoi d&rsquo;Autéquia !!<br />
+<a href="/?p=4855">AG 051</a> : VS Charmina ! Combat de Concours !!<br />
+<a href="/?p=4959">AG 052</a> : Posipi et Négapi ! La voie de l&rsquo;encouragement !?<br />
+<a href="/?p=4959">AG 053</a> : Skitty et Assistance ! Le ranch des Chamallot !!<br />
+<a href="/?p=6271">AG 054</a> : Team Magma vs Team Aqua, la revanche ! Le combat du Mont Chimnée !!<br />
+<a href="/?p=7278">AG 055</a> : La championne novice, Adriane ! Le terrain rempli de trous ?!<br />
+AG 056 : Le Badge Chaleur ! Combat enflammé pour l&rsquo;obtenir !!</p>
+			</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+			</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
