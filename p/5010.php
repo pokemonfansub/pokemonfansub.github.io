033a18a7def6f266f476b81a00c1b434d5f63c64
andykimpe1 <andykimpe@gmail.com>
2026-01-13 01:36:06 +0100

    Create 5010.php

---
+++ p/5010.php
@@ -0,0 +1,33 @@
+<article id="post-5010" class="post-5010 post type-post status-publish format-standard has-post-thumbnail hentry category-release">
+	<header class="entry-header entry-header-single">
+		<h1 class="entry-title">Pokémon Film 22 VOSTFR</h1>		<div class="entry-meta entry-meta-1"><span class="entry-date"><time class="entry-date published" datetime="2020-02-04T00:53:40+00:00">4 février 2020</time></span></div>	</header><!-- .entry-header -->
+
+
+	<div class="entry-content">
+		<p>Bonjour,</p>
+<p>Le film 22 étant disponible en VOSTFR légalement sur Netflix, nous avons décidé de supprimer les liens du site et de nyaa.</p>
+<p>Nous rappelons que notre activité est amateure et illégale, donc privilégiez toujours le légal.</p>
+<p>Bonne journée à tous.</p>
+			</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<div class="entry-taxonomies"><div class="entry-categories"><span>Categories</span><ul class="post-categories">
+						<div class="post-navigation row">
+			<div class="col-md-6">
+								<span>Previous article</span>
+				<h2 class="h5"><a href="/?p=5008" rel="prev">Pocket Monsters 2019 010 VOSTFR</a></h2>
+							</div>
+			<div class="col-md-6 post-navi-next">
+								<span>Next article</span>
+				<h2 class="h5"><a href="/?p=5031" rel="next">Pocket Monsters 2019 011 VOSTFR</a></h2>
+							</div>
+		</div>
+		
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
+
+<!-- <div id="comments" class="comments-area"> -->
+					
+				</main><!-- #main -->
+			</div><!-- #primary -->
+<!--<article end>-->
