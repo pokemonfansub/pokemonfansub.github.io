352feb371ae04ddda49ec51aa477a3e6dee6e511
andykimpe1 <andykimpe@gmail.com>
2026-01-13 01:28:22 +0100

    Create 4214.php

---
+++ p/4214.php
@@ -0,0 +1,43 @@
+<article id="post-4214" class="post-4214 post type-post status-publish format-standard has-post-thumbnail hentry category-release">
+	<header class="entry-header entry-header-single">
+		<h1 class="entry-title">Pokémon Film 21 VOSTFR</h1>		<div class="entry-meta entry-meta-1"><span class="entry-date"><time class="entry-date published" datetime="2019-04-12T22:45:27+00:00">12 avril 2019</time></span></div>	</header><!-- .entry-header -->
+
+
+	<div class="entry-content">
+		<p>Bonjour à tous !</p>
+<p>Vous l&rsquo;attendiez depuis des mois et le voici enfin : le 21e film Pokémon en VOSTFR : &laquo;&nbsp;Pocket Monsters : Notre histoire à tous&nbsp;&raquo; !<br />
+(petite précision, nous avons traduit plus littéralement le titre japonais du film &laquo;&nbsp;Minna no Monogatari&nbsp;&raquo; au lieu d&rsquo;utiliser le titre de la VF &laquo;&nbsp;Le Pouvoir est en nous&nbsp;&raquo;)</p>
+<p>Exceptionnellement, nous vous proposons plusieurs choix de versions pour ce film :<br />
+&#8211; 720 ou 1080p<br />
+&#8211; Noms FR ou JAP (comme on le fait d&rsquo;habitude sur les épisodes)<br />
+&#8211; Couleur différente de sous-titre pour chaque perso (comme dans Sun &amp; Moon) ou blanc pour tout le monde (comme dans Advanced Génération)</p>
+<p>De plus, nous passons pour ce film en softsub. Autrement dit, les sous-titres ne sont pas &laquo;&nbsp;collés&nbsp;&raquo; aux épisodes et il y a plusieurs pistes disponibles par fichier, comme détaillé ci-dessous. Si vous ne vous y connaissez pas, le mieux est de télécharger la version de votre choix, de l&rsquo;ouvrir sous VLC et de sélectionner la piste qui vous intéresse dans le menu &laquo;&nbsp;Sous-titres&nbsp;&raquo;.<br />
+Et pour ceux qui sont perdus, nous avons aussi deux versions classiques (en hardsub) disponibles.</p>
+<p>Si vous souhaitez ré-utiliser nos sous-titres, vous pouvez, mais merci de nous créditer !</p>
+<p>Merci pour votre soutien et votre patience ! Et à présent, bon visionnage ! 😀</p>
+<p><img class="" src="https://archives.bulbagarden.net/media/upload/5/5d/M21_poster.png" alt="" width="686" height="385" /></p>
+<p><strong>Softsub</strong><br />
+<a href="https://mega.nz/#!pRRBSQSa!WbKNmR7XsS94kA0xtmGRzCVcUH8cGzlJQNqjxBlwX2Q">MEGA 720p</a> &#8211; <a href="https://mega.nz/#!dJI13QST!1dNlUeLxauQXHFVMnuOq_-NSgiR5QTZUQ2MnT3Z-Qs8">1080p</a><br />
+<a href="https://nyaa.si/view/1134826">Torrent 720p</a> &#8211; <a href="https://nyaa.si/view/1134827">1080p</a><br />
+<a href="https://uptobox.com/vgs8y35gskb1" target="_blank">UptoBox 1080p</a><br />
+Vous pouvez choisir entre FR/JAP et couleurs/sans couleur dans les pistes.</p>
+<p><strong>Hardsub</strong> (720p, avec les couleurs)<br />
+<a href="https://mega.nz/#!DgIhgYSC!uT_pgUxZ9gjN938qaNhC2QgIiu7wKAsEhEA0OqK9c60">MEGA FR</a> &#8211; <a href="https://mega.nz/#!b5QzBQ5R!rh393OqQoh3K_xBsXI6NWItyduKwwvHkZJ6ZXEb3axM"> JAP</a><br />
+<a href="https://nyaa.si/view/1134824">Torrent FR</a> &#8211; <a href="https://nyaa.si/view/1134825">JAP</a><br />
+<a href="https://uptobox.com/12oxtjac7ccv" target="_blank">UptoBox FR</a> &#8211; <a href="https://uptobox.com/enxc8abpqwza" target="_blank">JAP</a></p>
+			</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+						<div class="post-navigation row">
+			<div class="col-md-6">
+								<span>Previous article</span>
+				<h2 class="h5"><a href="/?p=4198" rel="prev">Pokémon Sun &#038; Moon 113 et 114 VOSTFR</a></h2>
+							</div>
+			<div class="col-md-6 post-navi-next">
+								<span>Next article</span>
+				<h2 class="h5"><a href="/?p=4210" rel="next">Pokémon Advanced Génération 022 VOSTFR</a></h2>
+							</div>
+		</div>
+		
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
