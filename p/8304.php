586cc4eda7d4747c475eeb0671d542ba3bf4fd40
andykimpe1 <andykimpe@gmail.com>
2026-01-13 01:54:56 +0100

    Update 8304.php

--- p/8304.php
+++ p/8304.php
@@ -1,19 +1,19 @@
 <!--<article star>-->
 <article id="post-8304" class="post-8304 post type-post status-publish format-standard has-post-thumbnail hentry category-pokemon-horizons category-release">
 	<header class="entry-header entry-header-single">
-		<h1 class="entry-title">Pokémon Horizons 115 et 116 VOSTFR</h1>		<div class="entry-meta entry-meta-1"><span class="author vcard"><a class="url fn n" href="https://pokemonfansub.alwaysdata.net/?author=2">Trizek</a></span><span class="entry-date"><time class="entry-date published updated" datetime="2025-12-04T04:33:34+00:00">4 décembre 2025</time></span><span class="comments-link"><i class="fa fa-comments-o"></i><a href="https://pokemonfansub.alwaysdata.net/?p=8304#comments">7</a></span></div>	</header><!-- .entry-header -->
+		<h1 class="entry-title">Pokémon Horizons 115 et 116 VOSTFR</h1>		<div class="entry-meta entry-meta-1"><span class="entry-date"><time class="entry-date published updated" datetime="2025-12-04T04:33:34+00:00">4 décembre 2025</time></span></div>	</header><!-- .entry-header -->
 
 
 	<div class="entry-content">
 		<p>Bonjour à tous !</p>
 <p>Les Électacleurs Volants prennent un nouvel envol, direction l&rsquo;Institut Myrtille à Unys ! Liko, Rhod, Dot et Uruto espèrent que la professeure Bria pourra leur apporter de nouvelles informations sur Terapagos. Mais ils espèrent aussi pouvoir s&rsquo;entraîner à l&rsquo;institut pour devenir plus forts !</p>
 <p>Bon visionnage !</p>
-<!--<p><img class="" src="https://pokemonfansub.alwaysdata.net/wp-content/uploads/HZ115.png" alt="" width="686" height="385" /></p>-->
+<!--<p><img class="" src="/wp-content/uploads/HZ115.png" alt="" width="686" height="385" /></p>-->
 <p>PMHZ 115, Pokémon en français :
 
 
 <!--<a href="https://mega.nz/file/edsEyayK#OyXWFgQ6228NJseplUkscIqo4HeP8BCoBqeUH_WvopI">Mega</a>-->
-<a href="https://pokemonfansub.alwaysdata.net/download/Pokemon-Fansub-Pokemon-Horizons-115-VOSTFR-FR-1920x1080-H264-AAC.mp4.php">Téléchargement direct</a>
+<a href="/download/Pokemon-Fansub-Pokemon-Horizons-115-VOSTFR-FR-1920x1080-H264-AAC.mp4.php">Téléchargement direct</a>
 
 
 
@@ -23,14 +23,14 @@
 et <a href="https://nyaa.si/view/2049816">Torrent</a><br />
 
 
-<iframe width="640" height="500" frameborder="0" src="https://pokemonfansub.alwaysdata.net/video/Pokemon-Fansub-Pokemon-Horizons-115-VOSTFR-FR-1920x1080-H264-AAC.mp4.html" allowfullscreen></iframe><br />
+<iframe width="640" height="500" frameborder="0" src="/video/Pokemon-Fansub-Pokemon-Horizons-115-VOSTFR-FR-1920x1080-H264-AAC.mp4.html" allowfullscreen></iframe><br />
 
 
 
 PMHZ 115, Pokémon en japonais :
 
 <!--<a href="https://mega.nz/file/fU9zRDQb#D5x8Mdi7zurjyd7nJF891N7aT3fNAlKY6tSd4XBxJJQ">Mega</a>-->
-<a href="https://pokemonfansub.alwaysdata.net/download/Pokemon-Fansub-Pokemon-Horizons-115-VOSTFR-JAP-1920x1080-H264-AAC.mp4.php">Téléchargement direct</a>
+<a href="/download/Pokemon-Fansub-Pokemon-Horizons-115-VOSTFR-JAP-1920x1080-H264-AAC.mp4.php">Téléchargement direct</a>
 
 
 
@@ -38,12 +38,12 @@ PMHZ 115, Pokémon en japonais :
 
 et <a href="https://nyaa.si/view/2049817">Torrent</a></p>
 
-<iframe width="640" height="500" frameborder="0" src="https://pokemonfansub.alwaysdata.net/video/Pokemon-Fansub-Pokemon-Horizons-115-VOSTFR-JAP-1920x1080-H264-AAC.mp4.html" allowfullscreen></iframe><br />
+<iframe width="640" height="500" frameborder="0" src="/video/Pokemon-Fansub-Pokemon-Horizons-115-VOSTFR-JAP-1920x1080-H264-AAC.mp4.html" allowfullscreen></iframe><br />
 
 
 
 
-<!--<p><img class="" src="https://pokemonfansub.alwaysdata.net/wp-content/uploads/HZ116.png" alt="" width="686" height="385" /></p>-->
+<!--<p><img class="" src="/wp-content/uploads/HZ116.png" alt="" width="686" height="385" /></p>-->
 <p>PMHZ 116, Pokémon en français :
 
 
@@ -51,7 +51,7 @@ et <a href="https://nyaa.si/view/2049817">Torrent</a></p>
 
 
 <!--<a href="https://mega.nz/file/rF0gVb5a#AjTwwxHB1cmF_t4OL_z0QzghtZDZnFWvX2FmmRxEejk">Mega</a>-->
-<a href="https://pokemonfansub.alwaysdata.net/download/Pokemon-Fansub-Pokemon-Horizons-116-VOSTFR-FR-1920x1080-H264-AAC.mp4.php">Téléchargement direct</a>
+<a href="/download/Pokemon-Fansub-Pokemon-Horizons-116-VOSTFR-FR-1920x1080-H264-AAC.mp4.php">Téléchargement direct</a>
 
 
 
@@ -60,7 +60,7 @@ et <a href="https://nyaa.si/view/2049818">Torrent</a><br />
 
 
 
-<iframe width="640" height="500" frameborder="0" src="https://pokemonfansub.alwaysdata.net/video/Pokemon-Fansub-Pokemon-Horizons-116-VOSTFR-FR-1920x1080-H264-AAC.mp4.html" allowfullscreen></iframe><br />
+<iframe width="640" height="500" frameborder="0" src="/video/Pokemon-Fansub-Pokemon-Horizons-116-VOSTFR-FR-1920x1080-H264-AAC.mp4.html" allowfullscreen></iframe><br />
 
 
 
@@ -70,14 +70,14 @@ PMHZ 116, Pokémon en japonais :
 
 
 <!--<a href="https://mega.nz/file/DJFWDaiZ#zujO0r8Biyi3rArgbyoG_bqNlj_ofjuBaWfFuXEjDgA">Mega</a>-->
-<a href="https://pokemonfansub.alwaysdata.net/download/Pokemon-Fansub-Pokemon-Horizons-116-VOSTFR-JAP-1920x1080-H264-AAC.mp4.php">Téléchargement direct</a>
+<a href="/download/Pokemon-Fansub-Pokemon-Horizons-116-VOSTFR-JAP-1920x1080-H264-AAC.mp4.php">Téléchargement direct</a>
 
 
 
 et <a href="https://nyaa.si/view/2049819">Torrent</a></p>
 
 
-<iframe width="640" height="500" frameborder="0" src="https://pokemonfansub.alwaysdata.net/video/Pokemon-Fansub-Pokemon-Horizons-116-VOSTFR-JAP-1920x1080-H264-AAC.mp4.html" allowfullscreen></iframe><br />
+<iframe width="640" height="500" frameborder="0" src="/video/Pokemon-Fansub-Pokemon-Horizons-116-VOSTFR-JAP-1920x1080-H264-AAC.mp4.html" allowfullscreen></iframe><br />
 
 
 
@@ -85,16 +85,16 @@ et <a href="https://nyaa.si/view/2049819">Torrent</a></p>
 
 	<footer class="entry-footer">
 		<div class="entry-taxonomies"><div class="entry-categories"><span>Categories</span><ul class="post-categories">
-	<li><a href="https://pokemonfansub.alwaysdata.net/?cat=21" rel="category">Pokémon Horizons</a></li>
-	<li><a href="https://pokemonfansub.alwaysdata.net/?cat=6" rel="category">Release</a></li></ul></div></div>
+	<li><a href="/?cat=21" rel="category">Pokémon Horizons</a></li>
+	<li><a href="/?cat=6" rel="category">Release</a></li></ul></div></div>
 						<div class="post-navigation row">
 			<div class="col-md-6">
 								<span>Previous article</span>
-				<h2 class="h5"><a href="https://pokemonfansub.alwaysdata.net/?p=8282" rel="prev">Pokémon Horizons 112, 113 et 114 VOSTFR</a></h2>
+				<h2 class="h5"><a href="/?p=8282" rel="prev">Pokémon Horizons 112, 113 et 114 VOSTFR</a></h2>
 							</div>
 			<div class="col-md-6 post-navi-next">
 								<span>Next article</span>
-				<h2 class="h5"><a href="https://pokemonfansub.alwaysdata.net/?p=8321" rel="next">Pokémon Horizons 117 et 118 VOSTFR</a></h2>
+				<h2 class="h5"><a href="/?p=8321" rel="next">Pokémon Horizons 117 et 118 VOSTFR</a></h2>
 							</div>
 		</div>
 		
