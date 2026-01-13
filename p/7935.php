5f8571e7e42ae63e52c226c79d5a70ed9082ee5e
andykimpe1 <andykimpe@gmail.com>
2026-01-13 01:45:29 +0100

    Update 7935.php

--- p/7935.php
+++ p/7935.php
@@ -1,25 +1,25 @@
 <!--<article star>-->
 <article id="post-7935" class="post-7935 post type-post status-publish format-standard has-post-thumbnail hentry category-pokemon-horizons category-release">
 	<header class="entry-header entry-header-single">
-		<h1 class="entry-title">Pokémon Horizons 071, 072 et 073 VOSTFR</h1>		<div class="entry-meta entry-meta-1"><span class="author vcard"><a class="url fn n" href="https://pokemonfansub.alwaysdata.net/?author=2">Trizek</a></span><span class="entry-date"><time class="entry-date published" datetime="2024-11-27T11:09:00+00:00">27 novembre 2024</time></span><span class="comments-link"><i class="fa fa-comments-o"></i><a href="https://pokemonfansub.alwaysdata.net/?p=7935#comments">9</a></span></div>	</header><!-- .entry-header -->
+		<h1 class="entry-title">Pokémon Horizons 071, 072 et 073 VOSTFR</h1>		<div class="entry-meta entry-meta-1"></span><span class="entry-date"><time class="entry-date published" datetime="2024-11-27T11:09:00+00:00">27 novembre 2024</time></span></div>	</header><!-- .entry-header -->
 
 
 	<div class="entry-content">
 		<p>Bonjour à tous !</p>
 <p>Les Électacleurs Volants sont arrivés à Septentria ! Ils se rendent au Lac Cristallin pour rencontrer la Professeure Bria. Que pourra-t-elle leur apprendre sur Terapagos ?</p>
 <p>Bon visionnage !</p>
-<!--<p><img class="" src="https://pokemonfansub.alwaysdata.net/wp-content/uploads/HZ071.png" alt="" width="686" height="385" /></p>-->
+<!--<p><img class="" src="/wp-content/uploads/HZ071.png" alt="" width="686" height="385" /></p>-->
 <p>PMHZ 071, Pokémon en français :
 
 <!--<a href="https://mega.nz/file/VXE3XC5T#fhFQmdN_DXL7FLFul76eMJfwiMTE5k2VugrqCSsqYow">Mega</a>,-->
-<a href="https://pokemonfansub.alwaysdata.net/download/Pokemon-Fansub-Pokemon-Horizons-071-VOSTFR-FR-1920x1080-H264-AAC.mp4.php">Téléchargement direct</a>
+<a href="/download/Pokemon-Fansub-Pokemon-Horizons-071-VOSTFR-FR-1920x1080-H264-AAC.mp4.php">Téléchargement direct</a>
 
 
 
 
 et <a href="https://nyaa.si/view/1904711">Torrent</a><br />
 
-<iframe width="640" height="500" frameborder="0" src="https://pokemonfansub.alwaysdata.net/video/Pokemon-Fansub-Pokemon-Horizons-071-VOSTFR-FR-1920x1080-H264-AAC.mp4.html" allowfullscreen></iframe><br />
+<iframe width="640" height="500" frameborder="0" src="/video/Pokemon-Fansub-Pokemon-Horizons-071-VOSTFR-FR-1920x1080-H264-AAC.mp4.html" allowfullscreen></iframe><br />
 
 
 PMHZ 071, Pokémon en japonais :
@@ -27,7 +27,7 @@ PMHZ 071, Pokémon en japonais :
 
 <!--<a href="https://mega.nz/file/9Sd0QDgT#fwRZV4NZ15j3uBr3BtJ4UtoRE3cCGFBA9aw95Y8gskk">Mega</a>-->
 
-<a href="https://pokemonfansub.alwaysdata.net/download/Pokemon-Fansub-Pokemon-Horizons-071-VOSTFR-JAP-1920x1080-H264-AAC.mp4.php">Téléchargement direct</a>
+<a href="/download/Pokemon-Fansub-Pokemon-Horizons-071-VOSTFR-JAP-1920x1080-H264-AAC.mp4.php">Téléchargement direct</a>
 
 
 
@@ -35,16 +35,16 @@ et <a href="https://nyaa.si/view/1904712">Torrent</a></p>
 
 
 
-<iframe width="640" height="500" frameborder="0" src="https://pokemonfansub.alwaysdata.net/video/Pokemon-Fansub-Pokemon-Horizons-071-VOSTFR-JAP-1920x1080-H264-AAC.mp4.html" allowfullscreen></iframe><br />
+<iframe width="640" height="500" frameborder="0" src="/video/Pokemon-Fansub-Pokemon-Horizons-071-VOSTFR-JAP-1920x1080-H264-AAC.mp4.html" allowfullscreen></iframe><br />
 
 
 
 
-<p><img class="" src="https://pokemonfansub.alwaysdata.net/wp-content/uploads/HZ072.png" alt="" width="686" height="385" /></p>
+<p><img class="" src="/wp-content/uploads/HZ072.png" alt="" width="686" height="385" /></p>
 <p>PMHZ 072, Pokémon en français :
 
 <a href="https://mega.nz/file/4SMhhCIb#jhYl8U4rYzPFbRUJc_S1PemC4xWVygPnUNIrfYbr1Y0">Mega</a>
-<a href="https://pokemonfansub.alwaysdata.net/download/Pokemon-Fansub-Pokemon-Horizons-072-VOSTFR-FR-1920x1080-H264-AAC.mp4.php">Téléchargement direct</a>
+<a href="/download/Pokemon-Fansub-Pokemon-Horizons-072-VOSTFR-FR-1920x1080-H264-AAC.mp4.php">Téléchargement direct</a>
 
 
 
@@ -57,7 +57,7 @@ et <a href="https://nyaa.si/view/1904713">Torrent</a><br />
 
 
 
-<iframe width="640" height="500" frameborder="0" src="https://pokemonfansub.alwaysdata.net/video/Pokemon-Fansub-Pokemon-Horizons-072-VOSTFR-FR-1920x1080-H264-AAC.mp4.html" allowfullscreen></iframe><br />
+<iframe width="640" height="500" frameborder="0" src="/video/Pokemon-Fansub-Pokemon-Horizons-072-VOSTFR-FR-1920x1080-H264-AAC.mp4.html" allowfullscreen></iframe><br />
 
 
 
@@ -69,7 +69,7 @@ PMHZ 072, Pokémon en japonais : <a href="https://mega.nz/file/de1TRBoS#h_QzRq-3
 
 <div><div class="spoiler-wrap"><div class="spoiler-head no-icon  collapsed" title="Expand">Streaming</div><div class="spoiler-body" style="display: none;"><br />
 PM 072 FR
-<p><img class="" src="https://pokemonfansub.alwaysdata.net/wp-content/uploads/HZ073.png" alt="" width="686" height="385" /></p>
+<p><img class="" src="/wp-content/uploads/HZ073.png" alt="" width="686" height="385" /></p>
 <p>PMHZ 073, Pokémon en français : <a href="https://mega.nz/file/tP8yVDqB#0LbrdIHUMGChPtr5cE574aRUELpI6s_62dOWOhn5cek">Mega</a>, <a href="https://darkibox.com/eubyk5znm4wq">DarkiBox </a>et <a href="https://nyaa.si/view/1904715">Torrent</a><br />
 PMHZ 073, Pokémon en japonais : <a href="https://mega.nz/file/Za1XDBYB#_GK5TsaWoH4DUQcIJ_1Af__1rKr47zVtwrx_4QGi9J0">Mega</a>, <a href="https://darkibox.com/uv5nnnrfvon2">DarkiBox </a>et <a href="https://nyaa.si/view/1904716">Torrent</a></p>
 
@@ -77,16 +77,16 @@ PMHZ 073, Pokémon en japonais : <a href="https://mega.nz/file/Za1XDBYB#_GK5TsaW
 
 	<footer class="entry-footer">
 		<div class="entry-taxonomies"><div class="entry-categories"><span>Categories</span><ul class="post-categories">
-	<li><a href="https://pokemonfansub.alwaysdata.net/?cat=21" rel="category">Pokémon Horizons</a></li>
-	<li><a href="https://pokemonfansub.alwaysdata.net/?cat=6" rel="category">Release</a></li></ul></div></div>
+	<li><a href="/?cat=21" rel="category">Pokémon Horizons</a></li>
+	<li><a href="/?cat=6" rel="category">Release</a></li></ul></div></div>
 						<div class="post-navigation row">
 			<div class="col-md-6">
 								<span>Previous article</span>
-				<h2 class="h5"><a href="https://pokemonfansub.alwaysdata.net/?p=7919" rel="prev">Pokémon Horizons 068, 069 et 070 VOSTFR</a></h2>
+				<h2 class="h5"><a href="/?p=7919" rel="prev">Pokémon Horizons 068, 069 et 070 VOSTFR</a></h2>
 							</div>
 			<div class="col-md-6 post-navi-next">
 								<span>Next article</span>
-				<h2 class="h5"><a href="https://pokemonfansub.alwaysdata.net/?p=7949" rel="next">Pokémon Horizons 074 et 075 VOSTFR</a></h2>
+				<h2 class="h5"><a href="/?p=7949" rel="next">Pokémon Horizons 074 et 075 VOSTFR</a></h2>
 							</div>
 		</div>
 		
