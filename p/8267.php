df698ab0750f56045f446e80f8bdbca2c926085f
andykimpe1 <andykimpe@gmail.com>
2026-01-14 09:42:08 +0100

    Update 8267.php

--- p/8267.php
+++ p/8267.php
@@ -9,13 +9,60 @@
 <p>Les Explorateurs ont tendu un piège aux Électacleurs Volants. Les voilà maintenant obligés d&rsquo;affronter les Six Héros, boostés par ce qu&rsquo;il semble être des Sphères de Puissance améliorées&#8230; Comment vont-ils s&rsquo;en sortir ?</p>
 <p>Bon visionnage !</p>
 <p>PS : La diffusion de l&rsquo;animé au Japon est actuellement en pause, elle reprendra le 10 octobre avec 3 épisodes flash-backs centrés sur Rhod et Uruto à Kalos !</p>
-<p><img class="" src="/wp-content/uploads/HZ110.png" alt="" width="686" height="385" /></p>
-<p>PMHZ 110, Pokémon en français : <a href="https://mega.nz/file/2AclGZza#zmSdT6cBUtn4JC-3W_cH3srWCXVJAExWhLiHm6Ml7ls">Mega</a> et <a href="https://nyaa.si/view/2026352">Torrent</a><br />
-PMHZ 110, Pokémon en japonais : <a href="https://mega.nz/file/Wc9WTQqI#gcVNd003VApHBgptYBu5umLs8iCg1d1XvXWBdsLv_KA">Mega</a> et <a href="https://nyaa.si/view/2026353">Torrent</a></p>
-<p><img class="" src="/wp-content/uploads/HZ111.png" alt="" width="686" height="385" /></p>
-<p>PMHZ 111, Pokémon en français : <a href="https://mega.nz/file/mIMmCIQL#WlCJAG1hKdtDcRVDKstNTK5bohvQ5K7EqMFGUQHEAGo">Mega</a> et <a href="https://nyaa.si/view/2026354">Torrent</a><br />
-PMHZ 111, Pokémon en japonais : <a href="https://mega.nz/file/nYExQDYS#g94sgDzPxMLCT2cBIcSnUouY44ihm93DXuYJcCxc8Bk">Mega</a> et <a href="https://nyaa.si/view/2026355">Torrent</a></p>
-<p>Streaming : le streaming est disponible sur Mega après quelques minutes de traitement.</p>
+<!--<p><img class="" src="/wp-content/uploads/HZ110.png" alt="" width="686" height="385" /></p>-->
+<p>PMHZ 110, Pokémon en français : 
+
+
+
+<!--<a href="https://mega.nz/file/2AclGZza#zmSdT6cBUtn4JC-3W_cH3srWCXVJAExWhLiHm6Ml7ls">Mega</a>-->
+<a href="/download/Pokemon-Fansub-Pokemon-Horizons-110-VOSTFR-FR-1920x1080-H264-AAC.mp4.php">Téléchargement direct</a>
+
+	
+ et <a href="https://nyaa.si/view/2026352">Torrent</a><br />
+
+<iframe width="640" height="500" frameborder="0" src="/video.php?link=/download/Pokemon-Fansub-Pokemon-Horizons-110-VOSTFR-FR-1920x1080-H264-AAC.mp4.php&poster=/wp-content/uploads/HZ110.png" allowfullscreen></iframe><br />
+
+
+PMHZ 110, Pokémon en japonais : 
+	
+
+<!--<a href="https://mega.nz/file/Wc9WTQqI#gcVNd003VApHBgptYBu5umLs8iCg1d1XvXWBdsLv_KA">Mega</a>-->
+<a href="/download/Pokemon-Fansub-Pokemon-Horizons-110-VOSTFR-JAP-1920x1080-H264-AAC.mp4.php">Téléchargement direct</a>
+
+	
+et <a href="https://nyaa.si/view/2026353">Torrent</a></p>
+
+<iframe width="640" height="500" frameborder="0" src="/video.php?link=/download/Pokemon-Fansub-Pokemon-Horizons-110-VOSTFR-JAP-1920x1080-H264-AAC.mp4.php&poster=/wp-content/uploads/HZ110.png" allowfullscreen></iframe><br />
+
+
+		
+<!--<p><img class="" src="/wp-content/uploads/HZ111.png" alt="" width="686" height="385" /></p>-->
+
+
+<p>PMHZ 111, Pokémon en français :
+
+<!--<a href="https://mega.nz/file/mIMmCIQL#WlCJAG1hKdtDcRVDKstNTK5bohvQ5K7EqMFGUQHEAGo">Mega</a>-->
+<a href="/download/Pokemon-Fansub-Pokemon-Horizons-111-VOSTFR-FR-1920x1080-H264-AAC.mp4.php">Téléchargement direct</a>
+
+
+et <a href="https://nyaa.si/view/2026354">Torrent</a><br />
+
+<iframe width="640" height="500" frameborder="0" src="/video.php?link=/download/Pokemon-Fansub-Pokemon-Horizons-111-VOSTFR-FR-1920x1080-H264-AAC.mp4.php&poster=/wp-content/uploads/HZ111.png" allowfullscreen></iframe><br />
+
+
+PMHZ 111, Pokémon en japonais :
+
+<!--<a href="https://mega.nz/file/nYExQDYS#g94sgDzPxMLCT2cBIcSnUouY44ihm93DXuYJcCxc8Bk">Mega</a>-->
+<a href="/download/Pokemon-Fansub-Pokemon-Horizons-111-VOSTFR-JAP-1920x1080-H264-AAC.mp4.php">Téléchargement direct</a>
+
+
+
+et <a href="https://nyaa.si/view/2026355">Torrent</a></p>
+
+<iframe width="640" height="500" frameborder="0" src="/video.php?link=/download/Pokemon-Fansub-Pokemon-Horizons-111-VOSTFR-JAP-1920x1080-H264-AAC.mp4.php&poster=/wp-content/uploads/HZ111.png" allowfullscreen></iframe><br />
+
+
+
 			</div><!-- .entry-content -->
 	<footer class="entry-footer">
 		<div class="entry-taxonomies"><div class="entry-categories"><span>Categories</span><ul class="post-categories">
