c1ed3cc2a24e300491dcec9011d9797052313c46
andykimpe1 <andykimpe@gmail.com>
2026-01-14 12:38:33 +0100

    Create 8245.php

---
+++ p/8245.php
@@ -0,0 +1,62 @@
+<!--<article star>-->
+<article id="post-8245" class="post-8245 post type-post status-publish format-standard has-post-thumbnail hentry category-pokemon-horizons category-release">
+	<header class="entry-header entry-header-single">
+		<h1 class="entry-title">Pokémon Horizons 107 VOSTFR</h1>		<div class="entry-meta entry-meta-1"><span class="entry-date"><time class="entry-date published" datetime="2025-09-06T15:53:22+00:00">6 septembre 2025</time></span></div>	</header><!-- .entry-header -->
+
+
+	<div class="entry-content">
+		<p>Bonjour à tous !</p>
+<p>Uruto a de nouveau croisé la route des Explorateurs ! Il leur court après, tandis que Rhod part à sa recherche&#8230;</p>
+<p>Bon visionnage !</p>
+<!--<p><img class="" src="/wp-content/uploads/HZ107.png" alt="" width="686" height="385" /></p>-->
+<p>PMHZ 107, Pokémon en français :
+
+
+<!--<a href="https://mega.nz/file/6UMg1L4J#Avz6xPfMWFd5I8IqCgezMlORYNDCPWSEcDIDRyguQ6E">Mega</a>-->
+<a href="/download/Pokemon-Fansub-Pokemon-Horizons-107-VOSTFR-FR-1920x1080-H264-AAC.mp4.php">Téléchargement direct</a>
+
+
+et <a href="https://nyaa.si/view/2015941">Torrent</a><br />
+
+  
+<iframe width="640" height="500" frameborder="0" src="/video.php?link=/download/Pokemon-Fansub-Pokemon-Horizons-107-VOSTFR-FR-1920x1080-H264-AAC.mp4.php&poster=/wp-content/uploads/HZ107.png" allowfullscreen></iframe><br />
+  
+  
+PMHZ 107, Pokémon en japonais :
+
+
+<!--<a href="https://mega.nz/file/ONcRTR4L#7VtWJK0X0HtkEnUJGp8kOUdjQWmOTrgU53iUJsaSRVo">Mega</a>-->
+<a href="/download/Pokemon-Fansub-Pokemon-Horizons-107-VOSTFR-JAP-1920x1080-H264-AAC.mp4.php">Téléchargement direct</a>
+
+  
+et <a href="https://nyaa.si/view/2015942">Torrent</a></p>
+
+<iframe width="640" height="500" frameborder="0" src="/video.php?link=/download/Pokemon-Fansub-Pokemon-Horizons-107-VOSTFR-JAP-1920x1080-H264-AAC.mp4.php&poster=/wp-content/uploads/HZ107.png" allowfullscreen></iframe><br />
+
+
+			</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<div class="entry-taxonomies"><div class="entry-categories"><span>Categories</span><ul class="post-categories">
+	<li><a href="/?cat=21" rel="category">Pokémon Horizons</a></li>
+						<div class="post-navigation row">
+			<div class="col-md-6">
+								<span>Previous article</span>
+				<h2 class="h5"><a href="/?p=8235" rel="prev">Pokémon Horizons 106 VOSTFR</a></h2>
+							</div>
+			<div class="col-md-6 post-navi-next">
+								<span>Next article</span>
+				<h2 class="h5"><a href="/?p=8254" rel="next">Pokémon Horizons 108 et 109 VOSTFR</a></h2>
+							</div>
+		</div>
+		
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
+
+					
+<!-- <div id="comments" class="comments-area"> -->
+
+				
+				</main><!-- #main -->
+			</div><!-- #primary -->
+<!--<article end>-->
