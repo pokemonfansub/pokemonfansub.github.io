69ede50f7e3993266bfe4736e505571acd35220d
andykimpe1 <andykimpe@gmail.com>
2026-01-14 12:00:06 +0100

    Create 8254.php

---
+++ p/8254.php
@@ -0,0 +1,100 @@
+<!--<article star>-->
+<article id="post-8254" class="post-8254 post type-post status-publish format-standard has-post-thumbnail hentry category-pokemon-horizons category-release">
+	<header class="entry-header entry-header-single">
+		<h1 class="entry-title">Pokémon Horizons 108 et 109 VOSTFR</h1>		<div class="entry-meta entry-meta-1"><span class="entry-date"><time class="entry-date published updated" datetime="2025-09-19T18:42:20+00:00">19 septembre 2025</time></span></div>	</header><!-- .entry-header -->
+
+
+	<div class="entry-content">
+		<p>Bonjour à tous !</p>
+<p>La grand-mère de Liko, Diana, fait son grand retour ! De toute évidence, nos héros vont avoir beaucoup de choses à se raconter et à décider pour la suite&#8230;</p>
+<p>Bon visionnage !</p>
+<!--<p><img class="" src="/wp-content/uploads/HZ108.png" alt="" width="686" height="385" /></p>-->
+
+<p>PMHZ 108, Pokémon en français :
+
+<!--<a href="https://mega.nz/file/nFtGFIbI#z0dyx9Yz8arADMTJJ6OSHC3MZi6fzeooj9hs3rq023U">Mega</a>-->
+<a href="/download/Pokemon-Fansub-Pokemon-Horizons-108-VOSTFR-FR-1920x1080-H264-AAC.mp4.php">Téléchargement direct</a>
+
+
+et <a href="https://nyaa.si/view/2021519">Torrent</a><br />
+
+<iframe width="640" height="500" frameborder="0" src="/video.php?link=/download/Pokemon-Fansub-Pokemon-Horizons-108-VOSTFR-FR-1920x1080-H264-AAC.mp4.php&poster=/wp-content/uploads/HZ108.png" allowfullscreen></iframe><br />
+
+
+PMHZ 108, Pokémon en japonais :
+
+<!--<a href="https://mega.nz/file/PZ0XnRCa#VVBAnHg_AXib9TCPZdWdNYHijJPbZIA6NAiqdVvSNQo">Mega</a>-->
+<a href="/download/Pokemon-Fansub-Pokemon-Horizons-108-VOSTFR-JAP-1920x1080-H264-AAC.mp4.php">Téléchargement direct</a>
+
+
+
+et <a href="https://nyaa.si/view/2021520">Torrent</a></p>
+
+
+
+<iframe width="640" height="500" frameborder="0" src="/video.php?link=/download/Pokemon-Fansub-Pokemon-Horizons-108-VOSTFR-JAP-1920x1080-H264-AAC.mp4.php&poster=/wp-content/uploads/HZ108.png" allowfullscreen></iframe><br />
+
+
+
+
+
+<!--<p><img class="" src="/wp-content/uploads/HZ109.png" alt="" width="686" height="385" /></p>-->
+
+
+<p>PMHZ 109, Pokémon en français :
+
+<!--<a href="https://mega.nz/file/jUV2yYSB#rGKXiLj0HxADCCa_1tZ8fS56lcN1CTKTzIVAMt27-VQ">Mega</a>-->
+<a href="/download/Pokemon-Fansub-Pokemon-Horizons-109-VOSTFR-FR-1920x1080-H264-AAC.mp4.php">Téléchargement direct</a>
+
+
+
+
+et <a href="https://nyaa.si/view/2021521">Torrent</a><br />
+
+
+
+
+<iframe width="640" height="500" frameborder="0" src="/video.php?link=/download/Pokemon-Fansub-Pokemon-Horizons-109-VOSTFR-FR-1920x1080-H264-AAC.mp4.php&poster=/wp-content/uploads/HZ109.png" allowfullscreen></iframe><br />
+
+
+
+PMHZ 109, Pokémon en japonais :
+
+
+<a href="https://mega.nz/file/3QtyzRqa#CNHD2EJlCsPSXG7OCfcBk3-dIbQ87lIepSAph-W6x4Q">Mega</a>
+<a href="/download/Pokemon-Fansub-Pokemon-Horizons-109-VOSTFR-JAP-1920x1080-H264-AAC.mp4.php">Téléchargement direct</a>
+
+
+
+et <a href="https://nyaa.si/view/2021522">Torrent</a></p>
+
+<iframe width="640" height="500" frameborder="0" src="/video.php?link=/download/Pokemon-Fansub-Pokemon-Horizons-109-VOSTFR-JAP-1920x1080-H264-AAC.mp4.php&poster=/wp-content/uploads/HZ109.png" allowfullscreen></iframe><br />
+
+
+
+			</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<div class="entry-taxonomies"><div class="entry-categories"><span>Categories</span><ul class="post-categories">
+	<li><a href="/?cat=21" rel="category">Pokémon Horizons</a></li>
+						<div class="post-navigation row">
+			<div class="col-md-6">
+								<span>Previous article</span>
+				<h2 class="h5"><a href="/?p=8245" rel="prev">Pokémon Horizons 107 VOSTFR</a></h2>
+							</div>
+			<div class="col-md-6 post-navi-next">
+								<span>Next article</span>
+				<h2 class="h5"><a href="/?p=8267" rel="next">Pokémon Horizons 110 et 111 VOSTFR</a></h2>
+							</div>
+		</div>
+		
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
+
+					
+<!-- <div id="comments" class="comments-area"> -->
+
+				
+				</main><!-- #main -->
+			</div><!-- #primary -->
+<!--<article end>-->
